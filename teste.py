a40fc1c77d926cc5372c3b9634793d0897bb53cb
Gabriel-Schulze <gabriel.eduardoschulze20@gmail.com>
2024-10-16 22:14:33 -0300

    Testes

--- teste.py
+++ teste.py
@@ -1,5 +1,36 @@
-from crudUsuario import *
+import pandas as pd
+from pandastable import Table,TableModel
+from tkinter import *
+from crudFornecedor import *
 
-delete_usuario(2)
-print(read_usuario())
+
+class TestApp(Frame):
+    """Basic test frame for the table"""
+    def __init__(self, parent=None):
+        self.parent = parent
+        Frame.__init__(self)
+        self.main = self.master
+        self.main.geometry('600x400+200+100')
+        self.main.title('Table app')
+        f = Frame(self.main)
+        f.pack(fill=BOTH,expand=1)
+
+
+        data = read_fornecedor()
+        
+        df = pd.DataFrame({
+            "Nome": [x[1] for x in data],
+            "CNPJ": [x[2] for x in data],
+            "Endereco":[x[3] for x in data],
+            "Telefone": [x[4] for x in data]
+        })
+        
+        self.table = pt = Table(f, dataframe=df,editable=False)
+        pt.show()
+        return
+
+
+app = TestApp()
+#launch the app
+app.mainloop()
 
