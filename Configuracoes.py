7e451f38874d954103092c44f7aac94adb5524ac
Gabriel-Schulze <gabriel_eduardoschulze20@gmail.com>
2024-11-19 21:21:36 -0300

    começando pagina configuraçoes

--- Configuracoes.py
+++ Configuracoes.py
@@ -8,7 +8,17 @@ class Configuracoes:
         self.root = root
         self.root.title("Configurações")
         self.root.geometry('600x400+300+80')
-    
+
+        CTkLabel(self.root,text="Configurações",font=("Open Sans bold",28)).pack(pady=(30,10))
+
+        self.frame1 = Frame(self.root)
+        self.frame1.pack()
+        self.frame2 = Frame(self.root)
+        self.frame2.pack()
+        self.frame3 = Frame(self.root)
+        self.frame3.pack()
+        self.frame4 = Frame(self.root)
+        self.frame4.pack()
     
     
     
