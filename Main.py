73738eede55483c42585cc1deac83852bb72bbcc
devandrer <andre_r_borges@estudante.sesisenai.org.br>
2024-12-09 20:54:48 -0300

    front do menu e etc

--- Main.py
+++ Main.py
@@ -18,68 +18,68 @@ class Main:
         self.root.resizable(False, False)
         self.root.title("Pagina principal")
         self.root.geometry('1600x810+180+80')
-        self.root.config(background="darkblue")
+        self.root.config(background="#0322a2")
         
         self.fontMenu = ("Open Sans",22)
     
-        self.frame1 = Frame(self.root,background="darkblue")
+        self.frame1 = Frame(self.root,background="#0322a2")
         self.frame1.pack()
         
         self.criandoWidgets()
         
     def criandoWidgets(self):
         
-        self.frame2 = Frame(self.frame1,background="green")
+        self.frame2 = Frame(self.frame1,background="#223a9c")
         self.frame2.grid(column=1,row=1,rowspan=2)
         self.frame2.config(width=210,height=810)
         self.frame2.pack_propagate(False)
         
         
-        self.frameAdm = Frame(self.frame2,background="green")
+        self.frameAdm = Frame(self.frame2,background="#223a9c")
         self.frameAdm.grid(column=1,row=0,columnspan=2,pady=(10,0))
         
         logoAdm = CTkImage(light_image=Image.open("icons/admIcon.png"))
         
         CTkLabel(self.frameAdm, image=logoAdm,text="").grid(column=1,row=0,padx=(0,5))
         
-        CTkLabel(self.frameAdm, text=sys.argv[2],font=("Open Sans",19)).grid(column=2,row=0)
+        CTkLabel(self.frameAdm,text_color="#ffffff", text=sys.argv[2],font=("Open Sans",19)).grid(column=2,row=0)
             
         
-        self.inicioLink = CTkLabel(self.frame2,font=self.fontMenu ,text="Inicio")
+        self.inicioLink = CTkLabel(self.frame2,text_color="#ffffff",font=self.fontMenu ,text="Inicio")
         self.inicioLink.grid(column=1,row=1, columnspan=2,pady=(150,20))
         self.inicioLink.bind("<Button-1>", lambda e: self.telaInicio())
         
         if self.user == "admin":
-            self.usuarioLink = CTkLabel(self.frame2,font=self.fontMenu ,text="Usuario")
+            self.usuarioLink = CTkLabel(self.frame2,text_color="#ffffff",font=self.fontMenu ,text="Usuario")
             self.usuarioLink.grid(column=1,row=2, columnspan=2,pady=(10,20))
             self.usuarioLink.bind("<Button-1>", lambda e: self.telaUsuario())
         
-        self.produtoLink = CTkLabel(self.frame2,font=self.fontMenu ,text="Produtos")
+        self.produtoLink = CTkLabel(self.frame2,text_color="#ffffff",font=self.fontMenu ,text="Produtos")
         self.produtoLink.grid(column=1,row=3, columnspan=2,pady=(10,20))
         self.produtoLink.bind("<Button-1>", lambda e: self.telaProduto())
         
-        self.categLink = CTkLabel(self.frame2,font=self.fontMenu ,text="Categorias")
+        self.categLink = CTkLabel(self.frame2,text_color="#ffffff",font=self.fontMenu ,text="Categorias")
         self.categLink.grid(column=1,row=4, columnspan=2,pady=(10,20))
         self.categLink.bind("<Button-1>", lambda e: self.telaCategoria())
         
-        self.fornLink = CTkLabel(self.frame2,font=self.fontMenu ,text="Fornecedores",width=210)
+        self.fornLink = CTkLabel(self.frame2,text_color="#ffffff",font=self.fontMenu ,text="Fornecedores",width=210)
         self.fornLink.grid(column=1,row=5, columnspan=2,pady=(10,280))
         self.fornLink.bind("<Button-1>", lambda e: self.telaFornecedor())
 
         if self.user != "admin":
             self.fornLink.grid(column=1,row=5, columnspan=2,pady=(10,340))
         
-        self.frameConfig = Frame(self.frame2,background="green")
+        self.frameConfig = Frame(self.frame2,background="#223a9c")
         self.frameConfig.grid(column=1,row=6,columnspan=2)
         
         logoConfig = CTkImage(light_image=Image.open("icons/configureIcon.png"))
         
         CTkLabel(self.frameConfig, image=logoConfig,text="").grid(column=1,row=1,padx=(0,5))
-        self.configLink = CTkLabel(self.frameConfig,font=self.fontMenu ,text="Configurações")
+        self.configLink = CTkLabel(self.frameConfig,text_color="#ffffff",font=self.fontMenu ,text="Configurações")
         self.configLink.grid(column=2,row=1)
         
-        CTkLabel(self.frame2,text="Version 1.0",anchor=S).grid(column=1,row=7,sticky=W,padx=5)
-        self.btn_Logoof = CTkLabel(self.frame2 ,text="LogOff",font=("Open Sans",18),text_color="darkred")
+        CTkLabel(self.frame2,text_color="#ffffff",text="Version 1.0",anchor=S).grid(column=1,row=7,sticky=W,padx=5)
+        self.btn_Logoof = CTkLabel(self.frame2 ,text="LogOff",font=("Open Sans",18),text_color="red")
         self.btn_Logoof.grid(column=2,row=7,pady=(10,0))
         self.btn_Logoof.bind("<Button-1>", lambda e: self.logOff() )
         
